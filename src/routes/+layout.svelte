1dce6e2894febfe537269372cd6b02d1e6a77f59
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-03-12 10:57:50 +0100

    Add top bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
 	import '../app.css';
 </script>
 
+<div class="navbar bg-base-100 shadow-sm">
+	<div class="flex-1">
+		<a class="btn btn-ghost p-0 p-4 text-xl" href="/">
+			<img src="/logo.png" alt="Woeste Hoeve Logo" class="max-h-6" /> Woeste Hoeve DMX Control
+		</a>
+	</div>
+	<div class="flex-none"><p class="p-4">Vragen of kapot? Contact Ruben</p></div>
+</div>
+
 <slot></slot>
