c37a9d9f9d464ce32a999da4384fe20c4e682153
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-06-04 20:33:31 +0200

    Use 2 spaces instead of tabs for indentation

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -9,26 +9,26 @@ const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.js'],
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.js'],
 
-		languageOptions: {
-			parserOptions: {
-				svelteConfig
-			}
-		}
-	}
+    languageOptions: {
+      parserOptions: {
+        svelteConfig
+      }
+    }
+  }
 ];
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
-	import '../app.css';
+  import '../app.css';
 </script>
 
 <div class="navbar bg-base-100 shadow-sm">
-	<div class="flex-1">
-		<a class="btn btn-ghost p-0 p-4 text-xl" href="/">
-			<img src="/logo.png" alt="Woeste Hoeve Logo" class="max-h-6" /> Woeste Hoeve DMX Control
-		</a>
-	</div>
-	<div class="flex-none"><p class="p-4">Vragen of kapot? Contact Ruben</p></div>
+  <div class="flex-1">
+    <a class="btn btn-ghost p-0 p-4 text-xl" href="/">
+      <img src="/logo.png" alt="Woeste Hoeve Logo" class="max-h-6" /> Woeste Hoeve DMX Control
+    </a>
+  </div>
+  <div class="flex-none"><p class="p-4">Vragen of kapot? Contact Ruben</p></div>
 </div>
 
 <slot></slot>
