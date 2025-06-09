a63d764f1b988d9f806ead8dfa358a7d36e070e5
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-06-09 14:17:54 +0200

    Use cors and log when a value changes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const { setChannel } = require("./dmx");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/", (req, res) => {
   const { channel, value } = req.body;
+  console.log(`Setting channel ${channel} to value ${value}`);
   setChannel(channel, value);
   res.json({ success: true });
 });
