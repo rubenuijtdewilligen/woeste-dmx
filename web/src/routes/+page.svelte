34cd17bb43b0a9d4aa84a4280b054aec04b23eb5
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-06-09 16:38:18 +0200

    Update text on home page

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="mb-2 text-2xl font-bold">Welkom op het DMX controle paneel van de Woeste!</h1>
+
+<p>
+  De eerste versie van dit paneel is in 2025 ontwikkeld door Ruben Uijtdewilligen. Het doel is om
+  simpele lichtshows te programmeren om af en toe wat extra sfeer in de zaal te creëeren.
+</p>
+
+<p class="my-2">
+  Via het menu (op telefoon bovenaan, op computer aan de linkerzijde) kan je een fixture kiezen om
+  te bedienen.
+</p>
+
+<p>
+  Er is ook een pagina "Ruwe data sliders". Hierop zie je de DMX waarden die op dit moment in het
+  systeem zitten en deze kan je aanpassen via de sliders.
+</p>
+
+<p class="my-2">
+  Mocht je het interessant vinden om te zien hoe dit achter de schermen werkt, de source code van
+  het project (zowel dit dashboard als de controller die in de computerkast achter de bar ligt) is
+  beschikbaar op GitHub:
+  <a
+    href="https://github.com/rubenuijtdewilligen/woeste-dmx"
+    class="link link-primary"
+    target="_blank"
+  >
+    https://github.com/rubenuijtdewilligen/woeste-dmx
+  </a>.
+</p>
+
+<p class="text-gray-600"><i>Drink veel granen en blijf gezond!</i></p>
